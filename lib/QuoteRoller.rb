34350138183da668143d13ee7954d738bab51597
Stefan Streichsbier <stefan@streichsbier.at>
2014-04-09 20:49:07 +0700

    Fix typos

--- lib/QuoteRoller.rb
+++ lib/QuoteRoller.rb
@@ -6,7 +6,7 @@ require "QuoteRoller/resources/users"
 require "QuoteRoller/resources/proposals"
 require "QuoteRoller/resources/companies"
 require "QuoteRoller/resources/contacts"
-require "QuoteRoller/resources/catalog"
+require "QuoteRoller/resources/services"
 
 module QuoteRoller
   API_BASE_URL = "https://api.quoteroller.com/v2"
